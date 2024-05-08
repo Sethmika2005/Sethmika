409eb3274bf36609fd8c6f09c34c07d660f76820
Sethmika <sethmikabellana@gmail.com>
2024-05-08 11:53:52 +0530

    U[adte

---
+++ ppp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import numpy as np
+
+
+
+# Set Streamlit app title
+st.title("Minger Dashboard")
+
